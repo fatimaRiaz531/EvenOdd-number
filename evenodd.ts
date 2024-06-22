c1a01f769d9ddea5f325f1dc3f7f995b313cec3b
fatima <162475613+fatimaRiaz531@users.noreply.github.com>
2024-06-22 10:09:54 +0500

    CheckEvenOdd number

---
+++ evenodd.ts
@@ -0,0 +1,14 @@
+// Q1 : write a function 'checkEvenOdd' that accepts a number and prints whether it is even or odd?
+
+// solution:
+
+ function checkEvenOdd(num:number) {
+     if(num%2 ==0){
+        console.log("It's an even number.");  
+    }
+    else{
+        console.log("It's an odd number.")
+    }
+}
+checkEvenOdd(12);
+checkEvenOdd(5);
